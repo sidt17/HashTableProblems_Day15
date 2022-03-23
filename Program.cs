cdeec52285a3220b7100f55a458b5a09240621fa
Siddhant <santoshshrivastava39@gmail.com>
2022-03-23 22:51:41 +0530

    Call in main method

--- Program.cs
+++ Program.cs
@@ -6,20 +6,19 @@ namespace HashTable
     {
         static void Main(string[] args)
         {
+          
+            string paragraph = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+            string[] para = paragraph.Split(" ");
+            MyMapNode<int, string> hash = new MyMapNode<int, string>(para.Length);
+            int key = 0;
+            foreach (string word in para)
+            {
+                hash.Add(key, word);
+                key++;
+            }
+            Frequency frequency = new Frequency();
+            frequency.Freqncy(hash);
 
-            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
-            hash.Add("0", "to");
-            hash.Add("1", "be");
-            hash.Add("2", "or");
-            hash.Add("3", "not");
-            hash.Add("4", "to");
-            hash.Add("5", "be");
-
-
-            Console.WriteLine("Frequency of to is : " + hash.GetFrequencyOfWords("to"));
-            Console.WriteLine("Frequency of be is : " + hash.GetFrequencyOfWords("be"));
-            Console.WriteLine("Frequency of or is : " + hash.GetFrequencyOfWords("or"));
-            Console.WriteLine("Frequency of not is : " + hash.GetFrequencyOfWords("not"));
         }
     }
 }
