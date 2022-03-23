be4c040ccfa553fe5cff5217ad814ab3f0459b79
Siddhant <santoshshrivastava39@gmail.com>
2022-03-23 22:54:42 +0530

    Find frquency of words in para

---
+++ Frequency.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable
+{
+    class Frequency
+    {
+        Dictionary<string, int> frequency = new Dictionary<string, int>();
+        public void Freqncy(MyMapNode<int, string> hash)
+        {
+            for (int key = 0; key < hash.size; key++)
+            {
+                frequency.TryAdd(hash.Get(key).ToLower(), 0);
+                frequency[hash.Get(key).ToLower()]++;
+            }
+            foreach (KeyValuePair<string, int> item in frequency)
+            {
+                Console.WriteLine("Frequency of word " + item.Key + " is : " + item.Value);
+               
+            }
+        }
+    }
+}
